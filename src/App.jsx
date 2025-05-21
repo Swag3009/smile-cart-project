b879588a4ef80b3c7f940bf15dd0ed09bd5bcbd4
Swagatika <mswagatika3009@gmail.com>
2025-05-21 17:44:54 +0530

    Added product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,4 @@
-import "./App.css";
-import logo from "./logo";
+import Product from "./components/Product";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img alt="logo" className="App-logo" src={logo} />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
-
+const App = () => <Product />;
 export default App;
